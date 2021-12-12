4a1b0e1c68074c393cb0b349880fa0c485f2d49f
Iryna Babych <wilyreptiloid@gmail.com>
2021-12-12 16:34:10 +0200

    Edited color of picture

--- Lab2.py
+++ Lab2.py
@@ -1,3 +1,4 @@
+from matplotlib import colors
 import matplotlib.pyplot as plt
 import pandas as pd
 
@@ -5,5 +6,7 @@ array = pd.read_csv("DS9.txt", delimiter=' ',names=['x', 'y'])
 graphic = plt.figure(figsize=[5.4, 9.6])
 ax = graphic.add_subplot(111)
 ax.set_title("Data Set 9")
-ax.scatter(array.y, array.x)
+ax.set_ylabel("Second column")
+ax.set_xlabel("First column")
+ax.scatter(array.y, array.x, c=["#DD9787"])
 plt.show()
